c3f9cd3f379129b64a4842feb3358c49f6863cb1
mayur ghadigaonkar <65775923+mayur290997-hub@users.noreply.github.com>
2020-12-14 12:49:32 +0530

    first commit

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,9 @@
+  
 from django.contrib import admin
 from .models.product import Product
 from .models.category import Category
-# from .models.customer import Customer
-# from .models.orders import Order
+from .models.customer import Customer
+from .models.orders import Order
 
 
 class AdminProduct(admin.ModelAdmin):
@@ -17,3 +18,5 @@ class AdminCategory(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Product, AdminProduct)
 admin.site.register(Category , AdminCategory)
+admin.site.register(Customer )
+admin.site.register(Order )
